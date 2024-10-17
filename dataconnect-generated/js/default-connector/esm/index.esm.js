437b9e09138cbe69990608caba5e6d73f4c43f91
Silva Jamile (BD/ERA-AMS4) <sjv2ca@bosch.com>
2024-10-17 15:49:14 -0300

    teste

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Recrutamento',
+  location: 'us-central1'
+};
+
